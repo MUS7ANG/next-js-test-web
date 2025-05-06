364eb4c28e40f6d57ba6312c6b4fdaaf8fbb52ce
MUS7ANG <salihjudo@icloud.com>
2025-05-06 18:26:56 +0600

    local storage added

--- src/app/lib/store.ts
+++ src/app/lib/store.ts
@@ -1,4 +1,5 @@
 import {create} from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface CartProduct {
     id: number
@@ -10,21 +11,46 @@ interface CartProduct {
 interface CartStore {
     cart: CartProduct[],
     addToCart: (product: {id: number, name: string, price: number}) => void
+    removeFromCart: (id: number) => void
+    updateQuantity: (id: number, quantity: number) => void
+    clearCart: () => void
 }
 
-export const useCartStore = create<CartStore>((set) => ({
-    cart: [],
-    addToCart: (product) => {
-        set((state) => {
-            const isItemExists = state.cart.find((item) => item.id === product.id)
-            if (isItemExists) {
-                return {
+
+export const useCartStore = create<CartStore>()(
+    persist(
+        (set) => ({
+            cart: [],
+            addToCart: (product) => {
+                set((state) => {
+                    const isItemExists = state.cart.find((item) => item.id === product.id)
+                    if (isItemExists) {
+                        return {
+                            cart: state.cart.map((item) =>
+                                item.id === product.id ? {...item, quantity: item.quantity + 1} : item
+                            )
+                        }
+                    }
+                    return {cart: [...state.cart, {...product, quantity: 1}]}
+                })
+            },
+            removeFromCart: (id) =>
+                set((state) => ({
+                    cart: state.cart.filter((item) => item.id !== id),
+                })),
+
+            updateQuantity: (id, quantity) =>
+                set((state) => ({
                     cart: state.cart.map((item) =>
-                        item.id === product.id ? {...item, quantity: item.quantity + 1} : item
-                    )
-                }
-            }
-            return {cart: [...state.cart, {...product, quantity: 1}]}
-        });
-    }
-}))
+                        item.id === id ? { ...item, quantity: quantity > 0 ? quantity : 1 } : item
+                    ),
+                })),
+            clearCart: () => set({ cart: [] }),
+        }),
+        {
+            name: 'cart',
+            storage: createJSONStorage(() => localStorage),
+        }
+    )
+
+);
